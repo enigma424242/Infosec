cbc856fda537f4270087707f7c3d8253174e1e50
enigma <auxiliary42@gmail.com>
2016-07-31 01:48:23 -0400

    Create cleanlogs.sh

---
+++ cleanlogs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This shell script empties syslog-ng files
+
+# By Enigma
+
+if [ -f /var/log/auth.log ]
+	then
+	echo "" > /var/log/auth.log
+		if [ -f /var/log/auth.log.old ]
+		then
+		rm /var/log/auth.log.old
+		fi
+fi
+
+if [ -f /var/log/dmesg ]
+	then
+	echo "" > /var/log/dmesg
+		if [ -f /var/log/dmesg.old ]
+		then
+		rm /var/log/dmesg
+		fi
+fi
+
+if [ -f /var/log/messages ]
+	then
+	echo "" > /var/log/messages
+		if [ -f /var/log/messages.old ]
+		then
+		rm /var/log/messages.old
+		fi
+fi
