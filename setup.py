b7438cef3a8e6496c40313285c245131944eb125
enigma <auxiliary42@gmail.com>
2016-09-06 07:00:23 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'System Information',
+    'author': 'Engima',
+    'url': 'https://github.com/enigma424242/Infosec/blob/master/systeminformation.py'
+    'download_url': 'https://github.com/enigma424242/Infosec.git'
+    'author_email': 'auxiliary42@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': [],
+    'scripts': [],
+    'name': 'sysinfo'
+} 
